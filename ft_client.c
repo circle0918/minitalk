245d7319109d6e81a2b876ac85079d12344d160f
yyuan <yyuan@appledeMacBook-Air.local>
2021-11-07 22:03:40 +0100

    basic version

--- ft_client.c
+++ ft_client.c
@@ -6,13 +6,14 @@
 /*   By: yyuan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 15:54:59 by yyuan             #+#    #+#             */
-/*   Updated: 2021/11/07 21:39:39 by yyuan            ###   ########.fr       */
+/*   Updated: 2021/11/07 22:01:27 by yyuan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
+void ft_kill(int pid, char c);
 /*pid_t send_id(pid_t pid)
 {
 	static s_pid = pid;
@@ -23,41 +24,42 @@ void  client_action(int sig, siginfo_t *siginfo, void *text)
 	kill(s_pid, SIGUSER1);
 }*/
 
-/*void *ft_while(char *str)
-{
-}*/
-
-void *ft_kill(int pid, char *arg2)
+void ft_while(int pid, char *str)
 {
 	char c;
-//	kill(pid,SIGUSR1);
-	int i;
 	int j = 0;
 	
-	while(arg2[j]!= '\0')
+	while (str[j] != '\0')
 	{
-		c= arg2[j];
-//		printf("c:   %c\n",c);
-		i = 0;
-		while(i<8)
-		{
-			int check = c >> i& 1;
-//		printf ("check : %d\n", check);
-			if(check)   //拿到最低位 c向右移i位与1并。是否是1
-			{
-		 		kill(pid,SIGUSR1);
-//				printf("sent 1\n");
-			}
-			else
-			{
-				kill(pid,SIGUSR2);
-//			printf("sent 0\n");
-			}	
-			i++;
-			usleep(10);
-		}
+		c= str[j];
+		ft_kill(pid, c);
 		j++;
 	}
+
+}
+
+void ft_kill(int pid, char c)
+{
+	int i;
+//printf("c:   %c\n",c);
+  	i = 0;
+  	while(i<8)
+  	{
+  		int check = c >> i& 1;
+//printf ("check : %d\n", check);
+  		if(check)   //拿到最低位 c向右移i位与1并。是否是1
+  		{
+   			kill(pid,SIGUSR1);
+//		printf("sent 1\n");
+  		}
+  		else
+  		{
+  			kill(pid,SIGUSR2);
+//	printf("sent 0\n");
+  		}	
+  		i++;
+  		usleep(10);
+  	}
 }
 
 int main(int ac, char **av)
@@ -73,8 +75,8 @@ int main(int ac, char **av)
 	ft_putnbr_fd(ft_strlen(av[2]),1);
 	write(1, "\n", 1);
 // send_bit	
-	ft_kill(ft_atoi(av[1]), av[2]);
-	while (1)
-		pause();
+	ft_while(ft_atoi(av[1]), av[2]);
+//	while (1)
+//		pause();
 	return (0);
 }
